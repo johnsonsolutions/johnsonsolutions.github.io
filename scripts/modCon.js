d89499bb35ab21553a010fec93c6f0b4b62a9cff
JSOL <johnsontevin@live.com>
2025-02-16 15:31:43 -0500

    done

--- scripts/modCon.js
+++ scripts/modCon.js
@@ -10,60 +10,11 @@ let runway = {
 }
 
 $(document).ready(function(){
-    runway.add(fitting)
-    runway.add(testKit);
-    runway.add(force);
+    //runway.add();
     runway.run();
 });
 
 function isPortrait(){
     let size = {"x":screen.width, "y":screen.height };
     return size.x > size.y ? false : true;
-}
-
-function fitting(){
-    if(isPortrait()){
-        d3.select("#noForm")
-        .style('width', "67%");
-    }
-}
-
-
-class cUnit{
-    constructor(
-        navigation="",
-        search="",
-        bgVid="",
-        slider="",
-        cta="",
-        socials="",
-        footer=""
-    ){ }
-}
-
-let template = () =>{
-    let ret = new cUnit();
-
-    return ret;
-};
-
-
-
-function testKit(){
-    d3.select("body")
-    .append("p")
-    .attr('id', "demo");
-
-}
-
-function force(){
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("demo").innerHTML = this.responseText;
-      }
-    };
-    xhttp.open("GET", "kits/navigation/nav0.html", true);
-    xhttp.send();
 }
