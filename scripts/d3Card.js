1e5e0dede296f43d115269c5e8db89d217268eb2
JSOL <johnsontevin@live.com>
2025-03-29 22:19:19 -0400

    Card Update

--- scripts/d3Card.js
+++ scripts/d3Card.js
@@ -57,7 +57,7 @@ d3.json('scripts/projData.json').then(jsonData => {
         .append("a")
         .attr("href", d => d.link)
         .attr("target", "_blank")
-        .html('View on GitHub <i class="fab fa-github"></i>'); // Use .html() to insert the icon
+        .html('View Project &nbsp;<i class="fa fa-arrow-right"></i>');
 
 
     // Initialize jQuery event handlers after D3 rendering
@@ -65,7 +65,6 @@ d3.json('scripts/projData.json').then(jsonData => {
 
         if(isPortrait()){
             $(".projCard").on("click", function(){
-                // Use the data attribute or find the link in the current card
                 const link = $(this).find('.proj-link a').attr('href');
                 window.open(link, '_blank');
             });
@@ -77,22 +76,17 @@ d3.json('scripts/projData.json').then(jsonData => {
             var portraitWidth = 15 * parseFloat(getComputedStyle(document.documentElement).fontSize);
             var landscapeWidth = 42 * parseFloat(getComputedStyle(document.documentElement).fontSize);
            
-            // Use a data attribute to track the current state
             if (!$this.data('isExpanded')) {
-                // Expand to landscape mode
                 $this.data('isExpanded', true);
                 $this.animate({ width: landscapeWidth + "px" }, 300, function() {
                     $this.find(".expanded-content").fadeIn(200);
                 });
-                // Change background image to desktop version
                 $this.css("background-image", `url('${$this.attr("desk-src")}')`);
             } else {
-                // Return to portrait mode
                 $this.data('isExpanded', false);
                 $this.find(".expanded-content").fadeOut(200, function() {
                     $this.animate({ width: portraitWidth + "px" }, 300);
                 });
-                // Change back to mobile background
                 $this.css("background-image", `url('${$this.attr("mobile-src")}')`);
             }
         });
@@ -101,13 +95,11 @@ d3.json('scripts/projData.json').then(jsonData => {
             var $this = $(this);
             var portraitWidth = 15 * parseFloat(getComputedStyle(document.documentElement).fontSize);
            
-            // Check the data attribute instead of width
             if ($this.data('isExpanded')) {
                 $this.data('isExpanded', false);
                 $this.find(".expanded-content").fadeOut(200, function() {
                     $this.animate({ width: portraitWidth + "px" }, 300);
                 });
-                // Change back to mobile background
                 $this.css("background-image", `url('${$this.attr("mobile-src")}')`);
             }
         });
